a88c8c43a13007c7f672908ecc4f84df61028540
danishkodemonkey <humberjini@hotmail.com>
2024-05-07 12:28:14 +0200

    fix: fix pathing for categories

--- routes/catalog.js
+++ routes/catalog.js
@@ -44,28 +44,28 @@ router.get('/items', item_controller.item_list);
 router.get('/', item_controller.index);
 
 // GET request for creating a new item
-router.get('/category/create', category_controller.category_create_get);
+router.get('/categories/create', category_controller.category_create_get);
 
 // POST request for creating a category
-router.post('/category/create', category_controller.category_create_post);
+router.post('/categories/create', category_controller.category_create_post);
 
 // NOTE --- references with dynamic URLs (e.g :id) MUST come after any static URLs to avoid match
 // conflicts! That means anything that interacts with a specific id category for instance.
 
 // GET request to delete a category
-router.get('/category/:id/delete', category_controller.category_delete_get);
+router.get('/categories/:id/delete', category_controller.category_delete_get);
 
 // POST request to delete an category
-router.post('/category/:id/delete', category_controller.category_delete_post);
+router.post('/categories/:id/delete', category_controller.category_delete_post);
 
 // GET request to update an category
-router.get('/category/:id/update', category_controller.category_update_get);
+router.get('/categories/:id/update', category_controller.category_update_get);
 
 // POST request to update an category
-router.post('/category/:id/update', category_controller.category_update_post);
+router.post('/categories/:id/update', category_controller.category_update_post);
 
 // GET request for ONE category
-router.get('/category/:id', category_controller.category_detail);
+router.get('/categories/:id', category_controller.category_detail);
 
 // GET request to list ALL categorys
 router.get('/categories', category_controller.category_list);
